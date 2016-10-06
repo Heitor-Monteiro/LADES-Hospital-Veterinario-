6f713ef209ea2d7765c58a8f82193695cd8b84ce
KennedySouza <kennedy.es.souza@gmail.com>
2016-10-06 15:57:48 -0300

    Classe de Teste - Hibernate

--- SIHV/src/java/MODELO/WavesTestClass.java
+++ SIHV/src/java/MODELO/WavesTestClass.java
@@ -19,12 +19,9 @@ import org.hibernate.transform.*;
 public class WavesTestClass {
     public static void main(String[] args){
         GenericoDAOImpl genericDao = new GenericoDAOImpl();
-        /*List<Pessoa> listaPessoa = genericDao.listIdName("cpf","37685942145");
-        Pessoa obj = listaPessoa.get(0);
-        System.out.println("ID = "+obj.getPkPessoa()+"  ||  NOME = "+obj.getNome());*/
-        List<String> listaPelagem = genericDao.getPelagemNames();
-        for(String obj : listaPelagem){
-            System.out.println(obj);
+        List<Object[]> resultado = genericDao.list("SELECT c.id.fkPessoa, c.id.pkCliente from Pessoa p, Cliente c where p.pkPessoa = c.id.fkPessoa");
+        for(Object[] obj : resultado){
+            System.out.println(obj[1]);
         }
     }
 }
