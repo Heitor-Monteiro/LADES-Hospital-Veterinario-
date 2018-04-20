06b7d61dc82429845129b5e68f283f73f2a3901b
Heitor Monteiro <heitorssx2@gmail.com>
2018-04-20 15:14:22 -0300

    Mudança para mostrar mais dados cadastrais do proprietário na impressão

--- SIHV/src/main/java/com/lades/sihv/controller/pesquisa/ListagemPessoa.java
+++ SIHV/src/main/java/com/lades/sihv/controller/pesquisa/ListagemPessoa.java
@@ -50,13 +50,18 @@ public class ListagemPessoa extends AbstractBean {
                 break;
         }
 
-        List<?> listaPessoa = getDaoGenerico().list("SELECT p.pkPessoa, p.nome, p.cpfCnpj from Pessoa p, Cliente c" + tipoCliente + " where " + joinTipoCli + " p.pkPessoa=c.id.fkPessoa and p.exclusaoLogica=0 and " + searchMode + " like " + search);
+        List<?> listaPessoa = getDaoGenerico().list("SELECT p.pkPessoa, p.nome, p.cpfCnpj, p.logra, p.casaNumero, p.bairro, p.cidade, p.complemento from Pessoa p, Cliente c" + tipoCliente + " where " + joinTipoCli + " p.pkPessoa=c.id.fkPessoa and p.exclusaoLogica=0 and " + searchMode + " like " + search);
         List<Pessoa> retornaPessoa = new ArrayList<>();
         for (Object[] obj : (List<Object[]>) listaPessoa) {
             Pessoa newPessoa = new Pessoa();
             newPessoa.setPkPessoa((int) obj[0]);
             newPessoa.setNome((String) obj[1]);
             newPessoa.setCpfCnpj((String) obj[2]);
+            newPessoa.setLogra((String) obj[3]);
+            newPessoa.setCasaNumero((String) obj[4]);
+            newPessoa.setBairro((String) obj[5]);
+            newPessoa.setCidade((String) obj[6]);
+            newPessoa.setComplemento((String) obj[7]);
             retornaPessoa.add(newPessoa);
         }
         System.out.println("BACK-END WARNING: LIST RETURNED! [ public List<Pessoa> listBySearchPESSOA(String searchMode, String search) ]");
